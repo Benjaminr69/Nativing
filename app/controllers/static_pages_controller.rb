458ed6bd8e1cb1cd66f848b03f17016dd556e329
Benjaminr69 <benjamin.richard010@gmail.com>
2018-05-07 15:54:50 +0200

    Pages statiques ajoutées

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
+  	@titre = "Accueil"
   end
 
   def help
+  	@titre = "Aide"
+  end
+
+  def about
+  	@titre = "A propos"
+  end
+
+  def contact
+  	@titre = "Contact"
   end
 end
