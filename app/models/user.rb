388753a5ad1b2662220c1d8c4e232db0557077b8
Benjaminr69 <benjamin.richard010@gmail.com>
2018-05-09 00:42:07 +0200

    reset mot de passe première partie

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token
+	attr_accessor :remember_token, :reset_token
 
 	before_save { self.email = email.downcase }
 	validates :fname, presence: true, length: { maximum: 50 }
@@ -40,4 +40,17 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+   # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
 end
+
